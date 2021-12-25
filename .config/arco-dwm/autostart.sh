22198c5fa640b07a27be866d1ec8039538ca3ee7
Shady Zekry <shadyzekry75@gmail.com>
2021-12-25 16:58:16 +0200

    Configure picom with basic details

--- .config/arco-dwm/autostart.sh
+++ .config/arco-dwm/autostart.sh
@@ -6,7 +6,7 @@ function run {
     $@&
   fi
 }
-run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
+#run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
 #run "xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal"
 #run "xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
 #run xrandr --output eDP-1 --primary --mode 1368x768 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
@@ -25,7 +25,7 @@ run "xfce4-power-manager"
 run "blueberry-tray"
 run "/usr/lib/xfce4/notifyd/xfce4-notifyd"
 run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
-picom -b  --config ~/.config/arco-dwm/picom.conf &
+picom -b &
 run "numlockx on"
 run "volumeicon"
 run slstatus &
--- .config/arco-dwm/config.h
+++ .config/arco-dwm/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
