2e19f8e4edecab67e261c98d4b13136c705f97a9
Shady Zekry <shadyzekry75@gmail.com>
2024-09-16 14:19:00 +0300

    Add copilot to vim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -857,6 +857,8 @@ require("lazy").setup({
 		end,
 	},
 
+	{ "github/copilot.vim" },
+
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
 	-- place them in the correct locations.
