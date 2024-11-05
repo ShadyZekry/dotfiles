d0269996a0185aa5ffaf9980c7594448e54253b6
Shady Zekry <shadyzekry75@gmail.com>
2024-11-05 13:10:02 +0200

    Add oil nvim to vimrc

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -624,6 +624,16 @@ require("lazy").setup({
 		},
 	},
 
+	{
+	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+	},
+
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
 	-- place them in the correct locations.
