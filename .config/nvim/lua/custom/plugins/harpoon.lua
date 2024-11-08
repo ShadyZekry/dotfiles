af9c34a74a3a87480db16f0210aaf3a98ff7a75d
Shady Zekry <shadyzekry75@gmail.com>
2024-11-08 17:21:37 +0200

    Add harpoon to nvim configs

---
+++ .config/nvim/lua/custom/plugins/harpoon.lua
@@ -0,0 +1,22 @@
+return {
+	"ThePrimeagen/harpoon",
+	config = function()
+		require("telescope").load_extension("harpoon")
+		require("harpoon").setup({
+			menu = {
+				width = vim.api.nvim_win_get_width(0) - 4,
+			},
+		})
+
+		local ui = require("harpoon.ui")
+		vim.keymap.set("n", "<c-h>", ui.toggle_quick_menu, { desc = "Toggle Harpoon Menu" })
+		vim.keymap.set("n", "<c-j>", ui.nav_next, { desc = "Navigate to next harpoon mark" })
+		vim.keymap.set("n", "<c-k>", ui.nav_prev, { desc = "Navigate to prev harpoon mark" })
+		vim.keymap.set(
+			"n",
+			"<leader>h",
+			require("harpoon.mark").add_file,
+			{ desc = "Add a harpoon mark to current buffer" }
+		)
+	end,
+}
