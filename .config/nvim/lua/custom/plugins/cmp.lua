4bc15598e39ff540f72e0e655e6ab7c5d2b9d8e3
Shady Zekry <shadyzekry75@gmail.com>
2024-12-05 10:31:09 +0200

    Remove unwanted luasnip build for vim

--- .config/nvim/lua/custom/plugins/cmp.lua
+++ .config/nvim/lua/custom/plugins/cmp.lua
@@ -79,15 +79,6 @@ return { -- Autocompletion
 		-- Snippet Engine & its associated nvim-cmp source
 		{
 			"L3MON4D3/LuaSnip",
-			build = (function()
-				-- Build Step is needed for regex support in snippets.
-				-- This step is not supported in many windows environments.
-				-- Remove the below condition to re-enable on windows.
-				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-					return
-				end
-				return "make install_jsregexp"
-			end)(),
 			dependencies = {
 				-- `friendly-snippets` contains a variety of premade snippets.
 				--    See the README about individual language/framework/plugin snippets:
