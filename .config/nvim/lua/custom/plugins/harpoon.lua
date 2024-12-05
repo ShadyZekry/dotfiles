89b666fea96da9a39b2b0c3c1504fd263b6eb455
Shady Zekry <shadyzekry75@gmail.com>
2024-12-05 10:29:32 +0200

    Remove unwanted dependency in harpoon

--- .config/nvim/lua/custom/plugins/harpoon.lua
+++ .config/nvim/lua/custom/plugins/harpoon.lua
@@ -1,7 +1,6 @@
 return {
 	"ThePrimeagen/harpoon",
 	config = function()
-		require("telescope").load_extension("harpoon")
 		require("harpoon").setup({
 			menu = {
 				width = vim.api.nvim_win_get_width(0) - 4,
