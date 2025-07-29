f7a3e751decd645d97ffe8becb01ec4b9eb8c873
Shady Zekry <shadyzekry75@gmail.com>
2025-07-29 10:45:54 +0300

    Modify indentation backline thickness and underline

--- .config/nvim/lua/custom/plugins/indent.lua
+++ .config/nvim/lua/custom/plugins/indent.lua
@@ -3,5 +3,16 @@ return {
 	main = "ibl",
 	---@module "ibl"
 	---@type ibl.config
-	opts = {},
+	opts = {
+		indent = {
+			char = "▏", -- This is a slightly thinner char than the default one, check :help ibl.config.indent.char
+		},
+		scope = {
+			show_start = false,
+			show_end = false,
+		},
+	},
+	setup = function(_, opts)
+		require("ibl").setup(opts)
+	end,
 }
